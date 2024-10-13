3a3cacce6ac82b8dc3c89ba24bf9af02e893fa0e
dinhductai <161341324+dinhductai@users.noreply.github.com>
2024-10-13 10:09:52 +0700

    newcommit

--- ProjectLaptopStore/src/main/java/com/example/ProjectLaptopStore/Repository/Custom/IProductRepositoryCustom.java
+++ ProjectLaptopStore/src/main/java/com/example/ProjectLaptopStore/Repository/Custom/IProductRepositoryCustom.java
@@ -1,8 +1,6 @@
 package com.example.ProjectLaptopStore.Repository.Custom;
 
-import com.example.ProjectLaptopStore.DTO.Product_CreateProductDTO;
 import com.example.ProjectLaptopStore.DTO.Product_FindTopPurchasedProductsDTO;
-import com.example.ProjectLaptopStore.DTO.Product_UpdateProductDTO;
 import com.example.ProjectLaptopStore.Entity.ProductsEntity;
 
 import java.util.List;
