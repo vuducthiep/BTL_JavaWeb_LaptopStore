427e827c8dda3b2f1d633d57307ffbd0431123af
.january11th <sang1000000k@gmail.com>
2024-10-15 07:37:09 +0700

    sang-register-15/10

--- ProjectLaptopStore/src/main/java/com/example/ProjectLaptopStore/Service/IUserService.java
+++ ProjectLaptopStore/src/main/java/com/example/ProjectLaptopStore/Service/IUserService.java
@@ -1,7 +1,15 @@
 package com.example.ProjectLaptopStore.Service;
 
+import com.example.ProjectLaptopStore.DTO.User_RegisterDTO;
 import com.example.ProjectLaptopStore.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IUserService extends JpaRepository<UserEntity, Integer> {
+import java.util.List;
+
+public interface IUserService {
+    List<User_RegisterDTO> getAllUsers();
+    void createUser(User_RegisterDTO user) ;
+    void updateUser(String phoneNumber, User_RegisterDTO user);
+    void deleteUser(String phoneNumber);
+    boolean Authenticate(String phoneNumber, String password);
 }
