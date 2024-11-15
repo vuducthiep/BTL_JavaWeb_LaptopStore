19bb08a7351a8aa761ae9e28f0a94025ae90994d
.january11th <sang1000000k@gmail.com>
2024-11-15 10:37:21 +0700

    sang-updatepromo-15/11

--- ProjectLaptopStore/src/main/java/com/example/ProjectLaptopStore/Repository/IPromotionRepository.java
+++ ProjectLaptopStore/src/main/java/com/example/ProjectLaptopStore/Repository/IPromotionRepository.java
@@ -1,5 +1,6 @@
 package com.example.ProjectLaptopStore.Repository;
 
+import com.example.ProjectLaptopStore.DTO.Promotion_getPromotionProductDTO;
 import com.example.ProjectLaptopStore.Entity.PromotionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -23,4 +24,17 @@ public interface IPromotionRepository extends JpaRepository<PromotionEntity,Inte
             "LEFT JOIN promotions pm ON pm.PromotionID = pp.PromotionID\n" +
             "GROUP BY p.ProductID", nativeQuery = true)
     List<Object[]> getPromotionProduct(@Param("promotionID")int promotionID);
+
+    @Query(value = "SELECT p.ProductID, \n" +
+            "       p.ProductName AS productName, \n" +
+            "       p.Brand AS brand, \n" +
+            "       MAX(CASE WHEN pm.PromotionID = :promotionID THEN 1 ELSE 0 END) AS hasPromotion\n" +
+            "FROM Products p  \n" +
+            "LEFT JOIN promotionproduct pp ON p.ProductID = pp.ProductID\n" +
+            "LEFT JOIN promotions pm ON pm.PromotionID = pp.PromotionID\n" +
+            "WHERE p.ProductName like CONCAT('%', :productName, '%') \n "+
+            "GROUP BY p.ProductID", nativeQuery = true)
+    List<Object[]> searchProductByName(@Param("promotionID")int promotionID,
+                                                               @Param("productName")String productName);
+
 }
